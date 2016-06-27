25b857ab432bd81a1acf543816be6e8d0809048f
pancan <pxc0215@gmail.com>
2016-06-27 21:55:33 +0800

    '修改监听来电状态的类'

--- app/src/main/java/cn/pancan/callrecorder/helper/CallListener.java
+++ app/src/main/java/cn/pancan/callrecorder/helper/CallListener.java
@@ -1,13 +1,25 @@
 package cn.pancan.callrecorder.helper;
 
 import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 
 /**
- * Created by Administrator on 2016/6/27.
+ * Created by pan on 2016/6/27.
  */
 public class CallListener extends PhoneStateListener {
     @Override
     public void onCallStateChanged(int state, String incomingNumber) {
         super.onCallStateChanged(state, incomingNumber);
+        switch (state){
+            case TelephonyManager.CALL_STATE_IDLE:
+                System.out.println("挂断");
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                System.out.println("接听");
+                break;
+            case TelephonyManager.CALL_STATE_RINGING:
+                System.out.println("响铃:来电号码"+incomingNumber);
+                break;
+        }
     }
 }
--- app/src/main/java/cn/pancan/callrecorder/service/CallRecorderService.java
+++ app/src/main/java/cn/pancan/callrecorder/service/CallRecorderService.java
@@ -4,11 +4,21 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+import cn.pancan.callrecorder.helper.CallListener;
 
 /**
- * Created by Administrator on 2016/6/27.
+ * Created by pan on 2016/6/27.
  */
 public class CallRecorderService extends Service{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(CallRecorderService.this.TELEPHONY_SERVICE);
+        telephonyManager.listen(new CallListener(), PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -17,6 +27,7 @@ public class CallRecorderService extends Service{
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
+
     }
 
 }
