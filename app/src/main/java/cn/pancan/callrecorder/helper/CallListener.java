cb5966723cc81e681cb2fbe38075aadbea126bf9
bjpanxiecan <bjpanxiecan@corp.netease.com>
2016-07-25 15:58:06 +0800

    用小乌龟测试一下提交

--- app/src/main/java/cn/pancan/callrecorder/helper/CallListener.java
+++ app/src/main/java/cn/pancan/callrecorder/helper/CallListener.java
@@ -5,6 +5,8 @@ import android.os.Environment;
 import android.provider.Settings;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +14,7 @@ import java.io.IOException;
 /**
  * Created by pan on 2016/6/27.
  */
-public class CallListener extends PhoneStateListener {
+public class CallListener extends PhoneStateListener implements MediaRecorder.OnInfoListener, MediaRecorder.OnErrorListener{
     private String number;
     private boolean isRecord;
     private MediaRecorder recorder ;
@@ -20,9 +22,9 @@ public class CallListener extends PhoneStateListener {
     public CallListener() {
         this.recorder = new MediaRecorder();
         //recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);//存储格式
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);//设置编码
+//        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+//        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);//存储格式
+
 
     }
 
@@ -47,12 +49,20 @@ public class CallListener extends PhoneStateListener {
                     fileDir.mkdirs();
                 File file=new File(fileDir.getAbsolutePath(),recordTitle + ".amr");
 
-
+                recorder.reset();
+                recorder.setAudioSource(1);
+                recorder.setOutputFormat(1);//存储格式
+                recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);//设置编码
                 recorder.setOutputFile(file.getAbsolutePath());
+                recorder.setOnInfoListener(this);
+                recorder.setOnErrorListener(this);
+
                 try {
                     recorder.prepare();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Log.e("CallRecorder", "RecordService::onStart() IOException attempting recorder.prepare()\n");
+                    recorder = null;
+                    return;
                 }
                 recorder.start(); // 开始刻录
                 isRecord = true;
@@ -61,5 +71,18 @@ public class CallListener extends PhoneStateListener {
                 number=incomingNumber;
                 break;
         }
+    }
+
+    @Override
+    public void onError(MediaRecorder mr, int what, int extra) {
+        Log.e("CallRecorder", "RecordService got MediaRecorder onError callback with what: " + what + " extra: " + extra);
+        isRecord = false;
+        mr.release();
+    }
+
+    @Override
+    public void onInfo(MediaRecorder mr, int what, int extra) {
+        Log.i("CallRecorder", "RecordService got MediaRecorder onInfo callback with what: " + what + " extra: " + extra);
+        isRecord = false;
     }
 }
